b24d830f70db093e55139292dc8d37f60b4b02eb
unknown <luketpena@gmail.com>
2020-06-26 20:01:40 -0500

    Blend works

--- canvas-image-blend/canvas.js
+++ canvas-image-blend/canvas.js
@@ -12,11 +12,35 @@ function init() {
     canvas.height = window.innerHeight-32;
 
     let image = document.getElementById('mario');
+
+
+    //const newData = setToBlack(image.data);
+
     c.drawImage(image,0,0,image.width,image.height);
 
+    c.fillStyle = '#0ff';
+    c.globalCompositeOperation = "hue";
+    c.fillRect(0,0,canvas.width,canvas.height)
 
+    c.globalCompositeOperation = "destination-in";
+    c.drawImage(image,0,0,image.width,image.height);
+
+    image.hidden = true;
 }
 
+function setToBlack(data) {
+    let newData = [...data];
+    const n = data.length/4;
+
+    for (var i=0; i<n; i++) {
+        const pixel = i*4;
+        for (var j=0; j<3; j++) {
+            newData[i+j] = 0;
+        }
+    }
+
+    return newData;
+}
 
 
 
